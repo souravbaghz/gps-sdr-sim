1a0e024230c580d4dc217c435d1c7e2e1a4f0a19
Sourav Bagh <58340694+souravbaghz@users.noreply.github.com>
2023-03-14 14:17:27 +0530

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -4,6 +4,6 @@
 
 config_file="$1"
 
-cp bladeRF2-0.script "bladeRF_$config_file.script"
+cp bladeRF_2.0.script "bladeRF_2.0-$config_file.script"
 
-sed -i "s/tx config file=gpssim.bin format=bin/tx config file=$config_file format=bin/g" "bladeRF_$config_file.script"
+sed -i "s/tx config file=gpssim.bin format=bin/tx config file=$config_file format=bin/g" "bladeRF_2.0-$config_file.script"
